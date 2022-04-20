089ddfdd734f7db0d2b4ec05ed8e6c5fe679c28d
양태웅 <didxodnd0422@gmail.com>
2022-04-20 19:56:40 +0900

    Generate Project

---
+++ src/main/kotlin/com/patrick/Application.kt
@@ -0,0 +1,14 @@
+package com.patrick
+
+import io.ktor.server.application.*
+import com.patrick.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+    configureSerialization()
+    configureMonitoring()
+}
---
+++ src/test/kotlin/com/patrick/ApplicationTest.kt
@@ -0,0 +1,28 @@
+package com.patrick
+
+import io.ktor.server.routing.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.plugins.*
+import org.slf4j.event.*
+import io.ktor.server.request.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import kotlin.test.*
+import io.ktor.server.testing.*
+import com.patrick.plugins.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            configureRouting()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World!", bodyAsText())
+        }
+    }
+}
